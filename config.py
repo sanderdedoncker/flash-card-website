b7caa54b2cf31207b4602c252decda6225224221
sade <sander.dedoncker@empa.ch>
2022-04-10 10:57:16 +0200

    Prepare for first deployment

--- config.py
+++ config.py
@@ -6,12 +6,13 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
+# TODO: use config (sub)classing
 # App
-TESTING = True
+TESTING = False if environ.get('FLASK_ENV') == "production" else True
 EXPLAIN_TEMPLATE_LOADING = False
 SECRET_KEY = environ.get('SECRET_KEY')
 
 # Database
-SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI') if environ.get('FLASK_ENV') == "production" else environ.get('DEV_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = False if environ.get('FLASK_ENV') == "production" else True
